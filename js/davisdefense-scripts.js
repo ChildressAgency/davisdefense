99a64ba2831ecb3cfe805315d417ddbfd6d907e1
childressdev <jcampbell@childressagency.com>
2017-07-12 14:52:54 -0400

    styled first 4 section

--- js/davisdefense-scripts.js
+++ js/davisdefense-scripts.js
@@ -2,12 +2,24 @@ jQuery(document).ready(function($){
 
   if(typeof $.fn.fullpage == 'function'){
   	$('#hp-main').fullpage({
-			responsiveWidth: 768,
+			responsiveWidth:768,
       responsiveHeight:600,
+      slideSelector:'.fullpage-slide',
       verticalCentered: false,
       'onLeave': function(index, nextIndex, direction){
         $('.cap-link>span').addClass('animated bounce');
         $('.section1-img').addClass('animated slideInRight');
+        if(index == 2){
+          $('.client-logos ul').addClass('animated bounce');
+          $('.section2-img').addClass('animated slideInUp');
+        }
+        if(index == 3){
+          $('.teaming-vehicles').addClass('animated slideInUp');
+          $('.teaming-vehicles .row').addClass('animated bounce');
+        }
+        if(index == 4){
+          $('.blog-post-summary').addClass('animated slideInLeft');
+        }
       },
     });
   }
