8fa8a89b1a025474ae1bcd3ddb66345ffd993294
childressdev <jcampbell@childressagency.com>
2017-07-13 12:42:49 -0400

    capabilities unstyled

--- js/davisdefense-scripts.js
+++ js/davisdefense-scripts.js
@@ -27,4 +27,18 @@ jQuery(document).ready(function($){
   }
 
   new WOW().init();
+
+  $('.cap-link').on('click', function(e){
+    e.preventDefault;
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 136
+        }, 1000);
+        return false;
+      }
+    }    
+  });    
 });
