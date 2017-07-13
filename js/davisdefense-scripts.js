2fcff71682c7aacde65ec272860f4a8c974b0452
childressdev <jcampbell@childressagency.com>
2017-07-13 16:24:23 -0400

    capabilities done

--- js/davisdefense-scripts.js
+++ js/davisdefense-scripts.js
@@ -35,7 +35,7 @@ jQuery(document).ready(function($){
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top - 136
+          scrollTop: target.offset().top
         }, 1000);
         return false;
       }
