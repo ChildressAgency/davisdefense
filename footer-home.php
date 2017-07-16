9aee2b7b88ec112d7214d2fe7cae49d328a37bf7
John <jcampbell85.jc@gmail.com>
2017-07-15 21:46:47 -0400

    hp- moved footer section to front-page.php

--- footer-home.php
+++ footer-home.php
@@ -1,47 +1,6 @@
-      <section class="section fp-auto-height" id="section6">
-        <div class="footer">
-          <div class="login-section">
-            <div class="container">
-              <?php if(is_user_logged_in()): ?>
-                <div class="btn-inline">
-                  <a href="<?php echo home_url('employee-links'); ?>" class="btn-main">Employee Page</a>
-                  <a href="<?php echo wp_logout_url(home_url()); ?>" class="btn-main">Logout</a>
-                </div>
-              <?php else: ?>
-                <?php get_template_part('custom-login-form'); ?>
-              <?php endif; ?>
-            </div>
-          </div>
-          <div class="footer-nav">
-            <div class="container">
-              <a href="<?php echo home_url(); ?>" class="footer-logo pull-left">
-                <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/davis-logo-small.png" alt="Davis Defense Logo Small" />
-              </a>
-              <div class="footer-social pull-right">
-                <?php if(get_field('facebook', 'option')): ?>
-                  <a href="<?php the_field('facebook', 'option'); ?>" target="_blank"><i class="fa fa-facebook-official"></i></a>
-                <?php endif; 
-                if(get_field('twitter', 'option')): ?>
-                  <a href="<?php the_field('twitter', 'option'); ?>" target="_blank"><i class="fa fa-twitter"></i></a>
-                <?php endif;
-                if (get_field('linkedin', 'option')): ?>
-                  <a href="<?php the_field('linkedin', 'option'); ?>" target="_blank"><i class="fa fa-linkedin"></i></a>
-                <?php endif; ?>
-              </div>
-            </div>
-            <ul class="list-unstyled list-inline">
-              <li><a href="<?php echo home_url('capabilities'); ?>">Capabilities</a></li>
-              <li><a href="<?php echo home_url('about-us'); ?>">About Us</a></li>
-              <li><a href="<?php echo home_url('contracts'); ?>">Contracts</a></li>
-              <li><a href="<?php echo home_url('contact-us'); ?>">Contact Us</a></li>
-            </ul>
-            <div class="clearfix"></div>
-            <p class="copyright">&copy;<?php echo date('Y'); ?> Davis Defense Group</p>
-            <p class="copyright">website created by <a href="https://childressagency.com">The Childress Agency</a></p>
-          </div>
-        </div>
-      </section>
-    </main>
+<?php if(!is_front_page()): ?>
+
+<?php endif; ?>
     <?php wp_footer(); ?>
   </body>
 </html>
---
+++ front-page.php
@@ -0,0 +1,302 @@
+<?php get_header(); ?>
+<main id="hp-main">
+  <section class="section fp-auto-height-responsive" id="section0">
+    <img src="images/logo.png" class="img-responsive center-block animated slideInDown" alt="Davis Defense Logo" style="margin-top:40px;" />
+    <!--<div class="bottom-frame animated slideInUp">-->
+    <div class="company-awards-bg" style="background-image:url(images/home-header-image.png);">
+      <div class="company-awards">
+        <div class="container">
+          <ul class="list-unstyled list-inline animated bounce">
+            <li><img src="images/inc-500-1.png" class="img-responsive" alt="Inc 500 Logo" /></li>
+            <li><img src="images/fast-50-1.png" class="img-responsive" alt="Washington Technology Fast 50 Logo" /></li>
+            <li><img src="images/star-award-1.png" class="img-responsive" alt="Virginia's Fantastic 50 Star Award Logo" /></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+      <div class="quick-links">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-4">
+              <a class="media">
+                <div class="media-left media-middle">
+                  <span href="#" class="services-icon"></span>
+                </div>
+                <div class="media-body">
+                  <h3 class="media-heading">Services</h3>
+                  <p>We provide expert program support services - From acquisition analysis, to logistics, test and engineering, and business analysis.</p>
+                </div>
+              </a>
+            </div>
+            <div class="col-sm-4">
+              <a class="media">
+                <div class="media-left media-middle">
+                  <span href="#" class="contracts-icon"></span>
+                </div>
+                <div class="media-body">
+                  <h3 class="media-heading">Contracts</h3>
+                  <p>Davis defense is a premier education provider, supporting the United States Military Since 2002.</p>
+                </div>
+              </a>
+            </div>
+            <div class="col-sm-4">
+              <a class="media">
+                <div class="media-left media-middle">
+                  <span href="#" class="training-icon"></span>
+                </div>
+                <div class="media-body">
+                  <h3 class="media-heading">Training</h3>
+                  <p>There are many ways to contract with davis defense group, including via gsa schedules and seaport-e.</p>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!--</div>-->
+  </section>
+  <section class="section fp-auto-height-responsive" id="section1">
+    <div class="caps-icons keep-on-top">
+      <div class="container">
+        <h1>Capabilities</h1>
+        <div class="row">
+          <div class="col-sm-4 col-md-2">
+            <a href="#" class="cap-link">
+              <span class="business-icon"></span>
+              <h2>Business Analytics</h2>
+              <p>We provide a forward-leaning approach to data analysis that keeps our customers' financial and strategic objectives aligned with their missions.</p>
+            </a>
+          </div>
+          <div class="col-sm-4 col-md-2">
+            <a href="#" class="cap-link">
+              <span class="logistics-icon"></span>
+              <h2>Logistics</h2>
+              <p>Our acquisition logistics programs provide highly professional support for logistics documentation, training, risk management, equipment specialization, field maintenance, and tracking system analysis.</p>
+            </a>
+          </div>
+          <div class="col-sm-4 col-md-2">
+            <a href="#" class="cap-link">
+              <span class="engineering-icon"></span>
+              <h2>Engineering</h2>
+              <p>We provide the full spectrum of engineering and test support the entire acquisition lifecycle on myraid systems, from ground vehicles to C4ISR.</p>
+            </a>
+          </div>
+          <div class="clearfix visible-sm-block"></div>
+          <div class="col-sm-4 col-md-2">
+            <a href="#" class="cap-link">
+              <span class="cyber-icon"></span>
+              <h2>Cyber</h2>
+              <p>We provide the analysis and administration of your IT systems with a focus on mission advancement and system security.</p>
+            </a>
+          </div>
+          <div class="col-sm-4 col-md-2">
+            <a href="#" class="cap-link">
+              <span class="education-icon"></span>
+              <h2>Education</h2>
+              <p>Our worldwide support of military education and training programs sets the standard in the management and execution of distance learning programs.</p>
+            </a>
+          </div>
+          <div class="col-sm-4 col-md-2">
+            <a href="#" class="cap-link">
+              <span class="research-icon"></span>
+              <h2>Research &amp; Development</h2>
+              <p>Rapid prototyping and systems integration efforts that are tangible, worthwhile, and cost-effective solutions.</p>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section1-img"></div>
+  </section>
+  <section class="section fp-auto-height-responsive" id="section2">
+    <div class="client-logos keep-on-top">
+      <h1>Clients</h1>
+      <div class="container">
+        <ul class="list-unstyled list-inline">
+          <li><img src="images/marines-logo.png" class="img-responsive center-block" alt="Marines Logo" /></li>
+          <li><img src="images/army-logo.png" class="img-responsive center-block" alt="Army Logo" /></li>
+          <li><img src="images/homeland-security-logo.png" class="img-responsive center-block" alt="Homeland Security Logo" /></li>
+          <li><img src="images/navy-logo.png" class="img-responsive center-block" alt="Navy Logo" /></li>
+          <li><img src="images/air-force-logo.png" class="img-responsive center-block" alt="Air Force Logo" /></li>
+        </ul>
+      </div>
+    </div>
+    <div class="section2-img"></div>
+  </section>
+  <section class="section fp-auto-height-responsive" id="section3">
+    <div class="container">
+      <h1>Testimonials</h1>
+      <div id="testimonialSlider" class="carousel slide keep-on-top" data-ride="carousel">
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <div class="caption">
+              <p class="testimonial">"Davis Defense... met the highest standards set in the Quality Assurance Surveillance Plan."</p>
+              <p class="testimonial-author">-Program Manager, Engineer Systems (USMC)</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="caption">
+              <p class="testimonial">"The contractor did an exceptional job executing all tasks and deliverables fo PM AAA and PM MHTV either on-time or ahead of schedule. The contractor consistently worked under tight timelines and did an exceptional job responding to countless critical actions within the Program Offices."</p>
+              <p class="testimonial-author">-Program Manager, Amphibious Assault Vehicles and Program Manager, Medium and Heavy Tactical Vehicles (PEO LS).</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="caption">
+              <p class="testimonial">"The contractor did an exceptional job maintaining a full and operational staff not less than 95% of the time."</p>
+              <p class="testimonial-author">-Program Manager, Amphibious Assault Vehicles and Program Manager, Medium and Heavy Tactical Vehicles (PEO LS)</p>
+            </div>
+          </div>
+          <div class="item">
+            <div class="caption">
+              <p class="testimonial">"The attentive leadership and an extremely skilled workforce continued to make for a pleasant and productive experiences."</p>
+              <p class="testimonial-author">-Program Manager, Amphibious Assault Vehicles and Program Manager, Medium and Heavy Tactical Vehicles (PEO LS).</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="teaming-vehicles">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-6">
+            <h3>Teaming &amp; Business Opps</h3>
+            <a href="#" class="btn-main">Learn More</a>
+          </div>
+          <div class="col-sm-6">
+            <h3>Contract Vehicles</h3>
+            <a href="#" class="btn-main">Learn More</a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section3-img"></div>
+  </section>
+  <section class="section fp-auto-height-responsive" id="section4">
+    <div class="container">
+      <h1>Stay Updated</h1>
+      <div class="blog-post-summary">
+        <div class="media">
+          <div class="media-left">
+            <p>NOV 12</p>
+          </div>
+          <div class="media-body">
+            <h4 class="media-header">Title of blog post one<small>by john doe</small></h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, Lorem ipsum dolor sit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna...</p>
+            <a href="#" class="read-more">READ MORE</a>
+          </div>
+        </div>
+      </div>
+      <div class="blog-post-summary">
+        <div class="media">
+          <div class="media-left">
+            <p>NOV 12</p>
+          </div>
+          <div class="media-body">
+            <h4 class="media-header">Title of blog post one<small>by john doe</small></h4>
+            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, Lorem ipsum dolor sit. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna...</p>
+            <a href="#" class="read-more">READ MORE</a>
+          </div>
+        </div>
+      </div>
+      <div class="btn-inline">
+        <a href="#" class="btn-main btn-clear">Our Blog</a>
+        <a href="#" class="btn-main btn-clear">Subscribe</a>
+      </div>
+    </div>
+  </section>
+  <section class="section fp-auto-height-responsive" id="section5">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-5">
+          <h1>Contact Us</h1>
+        </div>
+        <div class="col-sm-7">
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="contact-info">
+                <h3>Address</h3>
+                <p>475 Aquia Towne Center Drive<br />Suite #401, Stafford, VA 22554</p>
+                <h3>Phone</h3>
+                <p>540.548.5980</p>
+                <h3>Fax</h3>
+                <p>540.542.3079</p>
+              </div>
+            </div>
+            <div class="col-sm-6">
+              <div class="footer-contact-form">
+                <div class="form-group">
+                  <label for="your-name" class="sr-only">Name</label>
+                  <input type="text" id="your-name" name="your-name" class="form-control" placeholder="NAME" />
+                </div>
+                <div class="form-group">
+                  <label for="phone" class="sr-only">Phone Number</label>
+                  <input type="text" id="phone" name="phone" class="form-control" placeholder="PHONE #" />
+                </div>
+                <div class="form-group">
+                  <label for="email" class="sr-only">Email</label>
+                  <input type="text" id="email" name="email" class="form-control" placeholder="EMAIL" />
+                </div>
+                <div class="form-group">
+                  <label for="subject" class="sr-only">What is this regarding</label>
+                  <input type="text" id="subject" name="subject" class="form-control" placeholder="WHAT IS THIS REGARDING" />
+                </div>
+                <div class="form-group">
+                  <label for="comments" class="sr-only">Comments</label>
+                  <textarea id="comments" name="comments" rows="4" class="form-control" placeholder="COMMENTS"></textarea>
+                </div>
+                <div class="form-group">
+                  <input type="submit" class="btn-main" value="SEND" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section5-img"></div>
+  </section>
+  <section class="section fp-auto-height" id="section6">
+    <div class="footer">
+      <div class="login-section">
+        <div class="container">
+          <?php if(is_user_logged_in()): ?>
+            <div class="btn-inline">
+              <a href="<?php echo home_url('employee-links'); ?>" class="btn-main">Employee Page</a>
+              <a href="<?php echo wp_logout_url(home_url()); ?>" class="btn-main">Logout</a>
+            </div>
+          <?php else: ?>
+            <?php get_template_part('custom-login-form'); ?>
+          <?php endif; ?>
+        </div>
+      </div>
+      <div class="footer-nav">
+        <div class="container">
+          <a href="<?php echo home_url(); ?>" class="footer-logo pull-left">
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/davis-logo-small.png" alt="Davis Defense Logo Small" />
+          </a>
+          <div class="footer-social pull-right">
+            <?php if(get_field('facebook', 'option')): ?>
+              <a href="<?php the_field('facebook', 'option'); ?>" target="_blank"><i class="fa fa-facebook-official"></i></a>
+            <?php endif; 
+            if(get_field('twitter', 'option')): ?>
+              <a href="<?php the_field('twitter', 'option'); ?>" target="_blank"><i class="fa fa-twitter"></i></a>
+            <?php endif;
+            if (get_field('linkedin', 'option')): ?>
+              <a href="<?php the_field('linkedin', 'option'); ?>" target="_blank"><i class="fa fa-linkedin"></i></a>
+            <?php endif; ?>
+          </div>
+        </div>
+        <ul class="list-unstyled list-inline">
+          <li><a href="<?php echo home_url('capabilities'); ?>">Capabilities</a></li>
+          <li><a href="<?php echo home_url('about-us'); ?>">About Us</a></li>
+          <li><a href="<?php echo home_url('contracts'); ?>">Contracts</a></li>
+          <li><a href="<?php echo home_url('contact-us'); ?>">Contact Us</a></li>
+        </ul>
+        <div class="clearfix"></div>
+        <p class="copyright">&copy;<?php echo date('Y'); ?> Davis Defense Group</p>
+        <p class="copyright">website created by <a href="https://childressagency.com">The Childress Agency</a></p>
+      </div>
+    </div>
+  </section>
+</main>
+<?php get_footer('home'); ?>
