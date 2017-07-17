cb72e3e20178f2a52470e211497ddcfc976943f5
childressdev <jcampbell@childressagency.com>
2017-07-17 12:19:43 -0400

    added logged in conditional

--- page-employee-links.php
+++ page-employee-links.php
@@ -5,12 +5,16 @@
       <div class="row">
         <div class="col-sm-6">
           <div class="emp-links-block">
-            <?php the_field('employee_links_intro'); ?>
-            <div class="emp-links">
-              <?php if(have_rows('employee_links')): while(have_rows('employee_links')): the_row(); ?>
-                <a href="<?php the_sub_field('link_address'); ?>" class="btn-emp-link"><?php the_sub_field('link_title'); ?></a>
-              <?php endwhile; endif; ?>
-            </div>
+            <?php if(is_user_logged_in()): ?>
+              <?php the_field('employee_links_intro'); ?>
+              <div class="emp-links">
+                <?php if(have_rows('employee_links')): while(have_rows('employee_links')): the_row(); ?>
+                  <a href="<?php the_sub_field('link_address'); ?>" class="btn-emp-link"><?php the_sub_field('link_title'); ?></a>
+                <?php endwhile; endif; ?>
+              </div>
+            <?php else: ?>
+              <p>You must be logged in to view this content.</p>
+            <?php endif; ?>
           </div>
         </div>
       </div>
